89ef42f7acbd2968177ad050a25f22cf5b77c2d1
pesingibhanusri <pesingibhanusri457@gmail.com>
2025-02-24 18:23:14 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+import Parent from './Components/Parent/Parent.jsx';
+
+
+function App() {
+
+
+  return (
+    <>
+  <Parent></Parent>
+    </>
+  )
+}
+
+export default App
